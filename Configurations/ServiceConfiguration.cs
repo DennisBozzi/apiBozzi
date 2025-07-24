209755ed8f44d99d29cc14018ef9e26591f7e7b6
DennisBozzi <dennisbozzii@gmail.com>
2025-07-23 22:40:47 -0300

    Aplicando template Swagger Dark DbContext Middleware and Service Configurations

---
+++ Configurations/ServiceConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using apiBozzi.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace apiBozzi.Configurations;
+
+public static class ServiceConfiguration
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient<string>();
+        
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+        
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        // services.AddScoped<IAuthInterface, AuthService>();
+        // services.AddScoped<IProdutoInterface, ProdutoService>();
+        // services.AddScoped<IVendaInterface, VendaService>();
+        
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAllOrigins",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+        });
+        
+        // services.AddAuthentication().AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+        // {
+        //     options.Authority = firebaseValidIssuer;
+        //     options.TokenValidationParameters = new TokenValidationParameters
+        //     {
+        //         ValidIssuer = firebaseValidIssuer,
+        //         ValidAudience = firebaseAudience,
+        //     };
+        // });
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Por favor, insira o token JWT com o prefixo 'Bearer '",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    []
+                }
+            });
+        });
+    }
+}
