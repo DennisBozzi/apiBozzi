796364161c9b36d0e72b448bec82a6e31e320d74
DennisBozzi <dennisbozzii@gmail.com>
2025-11-08 22:24:01 -0300

    refactor: changing demo-seed path

--- Controllers/FelicianoBozzi/DemoController.cs
+++ Controllers/FelicianoBozzi/DemoController.cs
@@ -20,15 +20,14 @@ public class DemoController : ControllerBase
         _env = env;
         _userProvider = userProvider;
     }
-
-    // Reinicia e popula os dados demo a partir do arquivo demo-seed.json na raiz do projeto
+    
     [HttpPost("ResetDemo")]
     public async Task<IActionResult> ResetDemo()
     {
         if (!_userProvider.IsAdmin)
             return Forbid();
 
-        var path = Path.Combine(_env.ContentRootPath, "demo-seed.json");
+        var path = Path.Combine(_env.ContentRootPath, "Utils/demo-seed.json");
         try
         {
             var result = await _seedService.SeedFromFileAsync(path);
